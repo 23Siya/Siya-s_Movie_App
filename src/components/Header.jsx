60b5a81c8a48995355815e62ae3cc8d4ec03c1d8
23Siya <156370624+23Siya@users.noreply.github.com>
2025-04-07 11:38:53 +0200

    Feature / header which is not full functional

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+function Header({ setCategory, setSelectedGenre }) {
+    return (
+      <header className="bg-blue-600 p-4 text-white">
+        <div className="container mx-auto flex justify-between items-center">
+          {/* App Title */}
+          <h1 className="text-2xl font-bold">Movie App</h1>
+  
+          {/* Navbar Component */}
+          <Navbar setCategory={setCategory} setSelectedGenre={setSelectedGenre} />
+        </div>
+      </header>
+    );
+  }
+  
+  export default Header;
